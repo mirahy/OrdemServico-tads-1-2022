6ba11df5afdfd8a9ac0af7e8caa6749254097af0
Mirahy <myrahy@gmail.com>
2022-04-13 19:36:35 -0400

    excluindo classes main denescessárias, criando inserts

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_tecnico (nome, telefone, email, senha) VALUES ('Tereza', '(67) 99999-8888', 'tereza@gmail.com', '1234');
+INSERT INTO tb_tecnico (nome, telefone, email, senha) VALUES ('Roberto', '(67) 99999-8888', 'roberto@gmail.com', '1234');
+INSERT INTO tb_tecnico (nome, telefone, email, senha) VALUES ('Ryan', '(67) 99999-8888', 'ryan@gmail.com', '1234');
+INSERT INTO tb_tecnico (nome, telefone, email, senha) VALUES ('Angelica', '(67) 99999-8888', 'angelica@gmail.com', '1234');
